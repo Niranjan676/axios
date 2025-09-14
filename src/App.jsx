ed585f0c168a2a6f6a957068ddafdd6bfafc8255
Niranjan <56575341+Niranjan676@users.noreply.github.com>
2025-09-14 19:39:55 +0000

    my pc-axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,69 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
 
   const [users, setUsers] = useState({ name: "", email: "", phone: "" })
   const [userdata, setUserData] = useState([])
+  const [edituser, setEdituser] = useState(false)
 
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users")
-    .then((res)=> setUserData(res.data))
-    .catch((err)=> console.log("Error", err))
-  },[])
+      .then((res) => setUserData(res.data))
+      .catch((err) => console.log("Error", err))
+  }, [])
 
-  const handleChange = (e) =>{
-    setUsers({...users, [e.target.id]: e.target.value})
+  const handleChange = (e) => {
+    setUsers({ ...users, [e.target.id]: e.target.value })
   }
 
-  const handleAddUser = () =>{
+  const handleAddUser = () => {
     axios.post("https://jsonplaceholder.typicode.com/users", users)
-    .then((res)=>{
-      let newUser = {...res.data, id: userdata.length === 0 ? 1 : userdata[userdata.length - 1].id + 1}
-      let userdatacopy = [...userdata]
-      userdatacopy.push(newUser)
-      setUserData(userdatacopy)
-      setUsers({ name: "", email: "", phone: "" })
-    })
-    .catch((err)=> console.log("Error", err))
+      .then((res) => {
+        let newUser = { ...res.data, id: userdata.length === 0 ? 1 : userdata[userdata.length - 1].id + 1 }
+        let userdatacopy = [...userdata]
+        userdatacopy.push(newUser)
+        setUserData(userdatacopy)
+        setUsers({ name: "", email: "", phone: "" })
+      })
+      .catch((err) => console.log("Error", err))
   }
 
-  const handleDelete = (id) =>{
+  const handleDelete = (id) => {
     axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-    .then((res)=>{
-      let usertodelete = [...userdata]
-      let checking = usertodelete.filter((userid)=> userid.id !== id)
-      setUserData(checking)
-    })
-    .catch((err)=> console.log("Error", err))
+      .then((res) => {
+        let usertodelete = [...userdata]
+        let checking = usertodelete.filter((userid) => userid.id !== id)
+        setUserData(checking)
+      })
+      .catch((err) => console.log("Error", err))
   }
 
 
   return (
     <div className='bg-gray-200 min-h-screen'>
       <div className='bg-cyan-950 h-12 flex items-center justify-center'>
-         <h1 className='text-white text-2xl'>AXIOS</h1>
+        <h1 className='text-white text-2xl'>AXIOS</h1>
       </div>
       <div className='container m-12 flex flex-row justify-between flex-wrap'>
         <div>
           <label className='text-xl mr-2' htmlFor='name'>Name: </label>
-          <input type='text' placeholder='Enter your name' id="name" value={users.name} className='p-2' onChange={handleChange}/>
+          <input type='text' placeholder='Enter your name' id="name" value={users.name} className='p-2' onChange={handleChange} />
         </div>
         <div>
           <label className='text-xl mr-2' htmlFor='email'>Email: </label>
-          <input type='email' placeholder='Enter your email address' id="email" value={users.email} className='p-2' onChange={handleChange}/>
+          <input type='email' placeholder='Enter your email address' id="email" value={users.email} className='p-2' onChange={handleChange} />
         </div>
         <div>
           <label className='text-xl mr-2' htmlFor='phone'>Phone: </label>
-          <input type='text' placeholder='Enter your phone' id="phone" value={users.phone} className='p-2' onChange={handleChange}/>
+          <input type='text' placeholder='Enter your phone' id="phone" value={users.phone} className='p-2' onChange={handleChange} />
         </div>
       </div>
       <div className='flex justify-center'>
         <button className='bg-blue-800 text-white px-4 py-2 text-xl rounded-md' onClick={handleAddUser}>Add user</button>
       </div>
-      <hr className='border border-slate-300 m-4 '/>
+      <hr className='border border-slate-300 m-4 ' />
       <div className='bg-white min-h-auto mx-4 rounded-md p-3'>
         <table className='table-auto w-full'>
           <thead>
@@ -76,16 +77,16 @@ function App() {
             </tr>
           </thead>
           <tbody>
-           {userdata.map((user)=>(
+            {userdata.map((user) => (
               <tr key={user.id}>
-              <td className='border border-slate-200 px-2 text-gray-500'>{user.id}</td>
-              <td className='border border-slate-200 px-2 text-gray-500'>{user.name}</td>
-              <td className='border border-slate-200 px-2 text-gray-500'>{user.email}</td>
-              <td className='border border-slate-200 px-2 text-gray-500'>{user.phone}</td>
-              <td className='border border-slate-200 text-center text-gray-500'><button className='bg-green-800 px-3 py-1 text-white rounded-sm'>Edit</button></td>
-              <td className='border border-slate-200 text-center text-gray-500'><button className='bg-red-800 px-3 py-1 text-white rounded-sm' onClick={()=>handleDelete(user.id)}>Delete</button></td>
-            </tr>
-           ))}
+                <td className='border border-slate-200 px-2 text-gray-500'>{user.id}</td>
+                <td className='border border-slate-200 px-2 text-gray-500'>{user.name}</td>
+                <td className='border border-slate-200 px-2 text-gray-500'>{user.email}</td>
+                <td className='border border-slate-200 px-2 text-gray-500'>{user.phone}</td>
+                <td className='border border-slate-200 text-center text-gray-500'><button className='bg-green-800 px-3 py-1 text-white rounded-sm'>Edit</button></td>
+                <td className='border border-slate-200 text-center text-gray-500'><button className='bg-red-800 px-3 py-1 text-white rounded-sm' onClick={() => handleDelete(user.id)}>Delete</button></td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
